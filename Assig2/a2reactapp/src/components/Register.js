99ec9900283c4ac7de1210a937e1ae0c86bccc22
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-10-24 22:58:44 +1030

    Trying to implement register. Right now it looks very similar to login, so doesn't deviate too much with how the data works. Currently, it does not route to login or anything like that, so further modification is needed

--- Assig2/a2reactapp/src/components/Register.js
+++ Assig2/a2reactapp/src/components/Register.js
@@ -1,19 +1,58 @@
 ﻿import React, { useState } from 'react';
+import SHA256 from 'crypto-js/sha256';
+import { Link, useNavigate } from "react-router-dom";
 
 const Register = ({ }) => {
+    const [userName, setUsername] = useState('');
+    const [pwd, setPwd] = useState('');
+    const navigate = useNavigate();
+    let url = 'http://localhost:5147/api/Register';
+
+    function onSubmit(e) {
+        e.preventDefault();
+
+        if (!userName || !pwd) {
+            alert("Username and Password are required!");
+            return;
+        }
+
+        fetch(`${url}?userName=${userName}&passwordHash=${SHA256(pwd).toString()}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data) {
+                    navigate("/Login");
+                } else {
+                    alert("Login failed");
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    function registerQuery(evt) {
+        const login = document.querySelector('[name="userName"]').value;
+        const pwd = document.querySelector('[name="password"]').value;
+        alert("Login: " + login);
+        setUsername(login);
+        alert("Password: " + pwd);
+        setPwd(pwd);
+    }
 
     return (
         <div className="card col-4 mb-2">
-            <h2>Login</h2>
-            <form method="post" className="row justify-content-start mb-3">
+            <h2>Register</h2>
+            <form method="post" onSubmit={onSubmit} className="row justify-content-start mb-3">
                 <div className="col-3">
-                    <input type="text" name="username" className="form-control" placeholder="Username" />
+                    <input type="text" name="userName" className="form-control" placeholder="Username" />
                 </div>
                 <div className="col-3">
                     <input type="text" name="password" className="form-control" placeholder="Password" />
                 </div>
                 <div className="col-3 text-left">
-                    <button type="submit">Register</button>
+                    <button type="submit" onClick={registerQuery}>Register</button>
+                    <Link className="nav-link" to="/Login">Login</Link>
                 </div>
             </form>
         </div>
