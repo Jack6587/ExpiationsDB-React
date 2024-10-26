06bb0615f487ca0b0abcfcb0b86e0d808ca88321
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-10-26 11:21:18 +1030

    Added the suburb dropdown component, currently working through that

---
+++ Assig2/a2reactapp/src/components/SuburbDropdown.js
@@ -0,0 +1,15 @@
+﻿import React, { useState } from 'react'
+
+function SuburbDropdown() {
+    const [suburbs, setSuburbs] = useState([]);
+
+    React.useEffect(() => {
+        console.log("useEffect");
+        fetch(`http://localhost:5147/api/Get_ListCameraSuburbs`)
+            .then(response => response.json())
+            .then(data => setSuburbs(data))
+            .catch(err => {
+                console.log(err);
+            });
+    }, [])
+}
