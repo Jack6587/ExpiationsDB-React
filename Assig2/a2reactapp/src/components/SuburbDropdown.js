c217652d25eb0ebc76569ff1ce1e4432ec71f5ed
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-10-27 12:05:39 +1030

    Updated the suburb drop down to make use of useEffect without React in front of it, and added an onSelect function that ties the selected value to a constant variable (aligned with a2 specs)

--- Assig2/a2reactapp/src/components/SuburbDropdown.js
+++ Assig2/a2reactapp/src/components/SuburbDropdown.js
@@ -1,9 +1,10 @@
-﻿import React, { useState } from 'react'
+﻿import React, { useState, useEffect } from 'react'
 
 function SuburbDropdown() {
     const [suburbs, setSuburbs] = useState([]);
+    const [selectedSuburb, setSelectedSuburb] = useState('');
 
-    React.useEffect(() => {
+    useEffect(() => {
         console.log("useEffect");
         fetch(`http://localhost:5147/api/Get_ListCameraSuburbs`)
             .then(response => response.json())
@@ -13,8 +14,12 @@ function SuburbDropdown() {
             });
     }, []);
 
+    function onSelect(event) {
+        setSelectedSuburb(event.target.value);
+    }
+
     return (
-        <select className="form-control">
+        <select className="form-control" value={selectedSuburb} onChange={onSelect}>
             <option value="">Select a suburb</option>
             {suburbs.map((suburb) => (
                 <option key={suburb} value={suburb}>
