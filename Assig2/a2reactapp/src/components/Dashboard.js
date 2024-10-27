27badf659ea0f1883e3a1b5972125c809fb0129a
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-10-27 23:45:08 +1030

    I added a basic nav bar that doesn't really work right now (the routes are correct but it looks awful) but I am just demonstrating here how the styling will start to look (and how I utilise css)

--- Assig2/a2reactapp/src/components/Dashboard.js
+++ Assig2/a2reactapp/src/components/Dashboard.js
@@ -1,10 +1,15 @@
 ﻿import React from 'react';
 import { Link } from 'react-router-dom';
 import SuburbDropdown from './SuburbDropdown';
+import './style/Navbar.css';
 
 const Dashboard = () => {
     return (
         <div className="dashboard">
+            <nav className="navbar">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/Report">Report</Link>
+            </nav>
             <h2>Dashboard</h2>
             <p>Search below...</p>
             <SuburbDropdown />
