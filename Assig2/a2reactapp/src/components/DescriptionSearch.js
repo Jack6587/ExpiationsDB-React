d9b6557c70baa71c2635702d5acc6f57a8dffef0
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-10-30 23:25:34 +1030

    Established a description search component, early stages + functionality right now

---
+++ Assig2/a2reactapp/src/components/DescriptionSearch.js
@@ -0,0 +1,16 @@
+﻿import React, { useState } from 'react';
+
+function DescriptionSearch() {
+    const [input, setInput] = useState('');
+
+    function onChange(event) {
+        const value = event.target.value;
+        setInput(value);
+    }
+
+    return (
+        <input type="text" placeholder="Search by description" value={input} onChange={onChange} />
+    );
+}
+
+export default DescriptionSearch;
