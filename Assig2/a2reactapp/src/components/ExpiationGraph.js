0e140d39eaec18ef5440b8bfaa97aeb04fcdf7dc
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-11-12 22:26:56 +1030

    Initialised the core data to be used for the graph, as well as specifics for the svg size and margins for axes

--- Assig2/a2reactapp/src/components/ExpiationGraph.js
+++ Assig2/a2reactapp/src/components/ExpiationGraph.js
@@ -3,6 +3,23 @@ import * as d3 from 'd3';
 
 const ExpiationGraph = ({ expiationDaysOfWeek }) => {
     const svgRef = useRef();
+
+    useEffect(() => {
+        const data = Object.entries(expiationDaysOfWeek).map(([day, count]) => ({
+            day,
+            count,
+        }));
+
+        const margin = { top: 20, right: 30, bottom: 40, left: 40 };
+        const width = 500 - margin.left - margin.right;
+        const height = 300 - margin.top - margin.bottom;
+
+        const svg = d3.select(svgRef.current)
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+    })
 }
 
 export default ExpiationGraph;
