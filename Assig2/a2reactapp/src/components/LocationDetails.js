2987d627940503c0bcaf8cc42f3a88e27ce006a7
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-11-12 21:59:01 +1030

    Technically this should be a completed location detail component for now - it gets the fetch and should display some data regarding the location (basic for now). Before I just moved everything from generic report over but that has changed and will be re-added later when I can ensure data is properly being passed

--- Assig2/a2reactapp/src/components/LocationDetails.js
+++ Assig2/a2reactapp/src/components/LocationDetails.js
@@ -5,38 +5,20 @@ const LocationDetails = ({ locationId, cameraTypeCode }) => {
 
     useEffect(() => {
         fetch(`http:localhost:5147/api/Get_ExpiationStatsForLocationId?locationId=${locationId}&cameraTypeCode=${cameraTypeCode}`)
-    })
+            .then(response => response.json())
+            .then(result => setData(result))
+            .catch(err => console.log(err));
+    }, [locationId, cameraTypeCode]);
 }
 
-return (
-    <div className="location-details">
-        <h2>Report for Locations: Location 1 and Location 2</h2>
-        <Link className="nav-link" to="/Dashboard">Return to Dashboard</Link>
-        <p><strong>Selected MPDC Sites</strong>:</p>
-        <ul>
-            <li>MPDC Site 1: Location A</li>
-            <li>MPDC Site 2: Location B</li>
-        </ul>
-        <p><strong>Filters used</strong>:</p>
-        <ul>
-            <li>Autocomplete Textbox for Offence Code Filter</li>
-            <li>Dropdown Box for Suburb Select</li>
-            <li>Dropdown Box for Specific Location Select</li>
-            <li>Date Range Select</li>
-        </ul>
-        <br />
-        <div>
-            {locationData.map((location) => {
-                const { locationId, data } = location;
-
-                return (
-                    <div key={locationId} style={{ marginBottom: '40px' }}>
-                        <div>
-                            <p>Total Offences: {data.totalOffences}</p>
-                        </div>
-                    </div>
-                );
-            })}
+    return (
+        <div className="location-details">
+            <h3>Location {locationId} Details</h3>
+            <div className="stats">
+                <p>Total Offences: {data.totalOffencesCount}</p>
+            </div>
         </div>
-    </div>
-)
+    );
+};
+
+export default LocationDetails;
