52c85adaf04da137b1be0a22d87f4b0c058a3953
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-11-12 21:54:40 +1030

    BIG changes to the code here. I just could not get the previous attempt working for getting multiple locations from the fetch request. It is theoretically logical but maybe I'm thinking about it the wrong way. So I'm taking a different approach. I'm breaking my code up into more manageable components, and the data can get passed that way. The report basically just stores the locations and passes that data to the location details component, which does the actual fetch for data. For the graphs, this will work a similar way, but I'm not there yet

---
+++ Assig2/a2reactapp/src/components/LocationDetails.js
@@ -0,0 +1,42 @@
+﻿import React, { useState, useEffect } from 'react';
+
+const LocationDetails = ({ locationId, cameraTypeCode }) => {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        fetch(`http:localhost:5147/api/Get_ExpiationStatsForLocationId?locationId=${locationId}&cameraTypeCode=${cameraTypeCode}`)
+    })
+}
+
+return (
+    <div className="location-details">
+        <h2>Report for Locations: Location 1 and Location 2</h2>
+        <Link className="nav-link" to="/Dashboard">Return to Dashboard</Link>
+        <p><strong>Selected MPDC Sites</strong>:</p>
+        <ul>
+            <li>MPDC Site 1: Location A</li>
+            <li>MPDC Site 2: Location B</li>
+        </ul>
+        <p><strong>Filters used</strong>:</p>
+        <ul>
+            <li>Autocomplete Textbox for Offence Code Filter</li>
+            <li>Dropdown Box for Suburb Select</li>
+            <li>Dropdown Box for Specific Location Select</li>
+            <li>Date Range Select</li>
+        </ul>
+        <br />
+        <div>
+            {locationData.map((location) => {
+                const { locationId, data } = location;
+
+                return (
+                    <div key={locationId} style={{ marginBottom: '40px' }}>
+                        <div>
+                            <p>Total Offences: {data.totalOffences}</p>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    </div>
+)
--- Assig2/a2reactapp/src/components/Report.js
+++ Assig2/a2reactapp/src/components/Report.js
@@ -1,54 +1,13 @@
-﻿import React, { useState, useEffect } from 'react';
+﻿import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 const Report = () => {
-    const [locationData, setLocationData] = useState([]);
-    const locationIds = [23, 1709];
-    const tempLocationData = useRef([]);
+    const locationIds = [
+        { locationId: 23, cameraTypeCode: 'M' },
+        { locationId: 1709, cameraTypeCode: 'M' },
+    ];
 
-    useEffect(() => {
-        const fetchLocationData = (index) => {
-            if (index >= locationIds.length) {
-                setLocationData()
-            }
-        }
 
-            const locationId = locationIds[index];
-            fetch(`http://localhost:5147/api/Get_ExpiationStatsForLocationId?locationId=${locationId}&cameraTypeCode=M`)
-                .then(response => response.json())
-                .then(data => {
-                    setLocationData((prevData) => [
-                        ...prevData,
-                        { locationId, data }
-                    ]);
-
-                    fetchLocationData(index + 1);
-                })
-                .catch(err => console.log(err));
-        };
-
-        fetchLocationData(0);
-
-    }, []);
-
-    return (
-        <div className="dashboard">
-            <h2>Report for Locations: Location 1 and Location 2</h2>
-            <Link className="nav-link" to="/Dashboard">Return to Dashboard</Link>
-            <p><strong>Selected MPDC Sites</strong>:</p>
-            <ul>
-                <li>MPDC Site 1: Location A</li>
-                <li>MPDC Site 2: Location B</li>
-            </ul>
-            <p><strong>Filters used</strong>:</p>
-            <ul>
-                <li>Autocomplete Textbox for Offence Code Filter</li>
-                <li>Dropdown Box for Suburb Select</li>
-                <li>Dropdown Box for Specific Location Select</li>
-                <li>Date Range Select</li>
-            </ul>
-        </div>
-    )
 }
 
 export default Report;
