c9d17bc7b8df7fa2a48c1f222147e491bba4d597
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-11-13 15:41:56 +1030

    Starting to implement the date filter which should not take long - just need to figure out time conversions from input to action. I also plan to do radio/tickboxes for day/night time filtering but at this point it's a matter of time constraints - if I don't have time, it's not the top priority and better to focus on UI, core functionality, etc

--- Assig2/a2reactapp/src/components/Dashboard.js
+++ Assig2/a2reactapp/src/components/Dashboard.js
@@ -3,12 +3,15 @@ import SuburbDropdown from './SuburbDropdown';
 import DescriptionSearch from './DescriptionSearch';
 import CameraSelect from './CameraSelect';
 import LocationResults from './LocationResults';
+import DateFilter from './DateFilter';
 
 const Dashboard = () => {
     const [selectedSuburb, setSelectedSuburb] = useState('');
     const [selectedCameraType, setSelectedCameraType] = useState('');
     const [searchQuery, setSearchQuery] = useState('');
     const [searchTrigger, setSearchTrigger] = useState(false);
+    const [startTime, setStartTime] = useState(0); // default start time value
+    const [endTime, setEndTime] = useState(2147483647);
 
     function handleSuburbChange(suburb) {
         setSelectedSuburb(suburb);
@@ -27,13 +30,18 @@ const Dashboard = () => {
         setSelectedCameraType(cameraType);
     }
 
+    function handleDateChange(newStartTime, newEndTime) {
+        setStartTime(newStartTime);
+        setEndTime(newEndTime);
+    }
+
     return (
         <div className="dashboard">
             <h2>Dashboard</h2>
             <SuburbDropdown searchQuery={searchQuery} onSuburbChange={handleSuburbChange} selectedSuburb={selectedSuburb} />
             <CameraSelect suburb={selectedSuburb} onCameraTypeChange={handleCameraTypeChange} />
             <DescriptionSearch onSearchChange={handleSearchChange} searchQuery={searchQuery} onSearchSubmit={handleSearchSubmit} />
-            <LocationResults suburb={selectedSuburb} cameraType={selectedCameraType} searchQuery={searchQuery} searchTrigger={searchTrigger} />
+            <LocationResults suburb={selectedSuburb} cameraType={selectedCameraType} searchQuery={searchQuery} searchTrigger={searchTrigger} startTime={startTime} endTime={endTime} />
 
         </div>
     );
---
+++ Assig2/a2reactapp/src/components/DateFilter.js
@@ -0,0 +1,8 @@
+﻿import React, { useState } from 'react';
+
+const DateFilter = ({ onDateChange }) => {
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+}
+
+export default DateFilter;
